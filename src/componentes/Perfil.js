4498a1ac901c0f781ced510d5207b95757d61a5f
emersonilaja <emerson.ilaja@inacapmail.cl>
2022-07-15 13:40:02 -0400

    Solución Full

--- src/componentes/Perfil.js
+++ src/componentes/Perfil.js
@@ -3,8 +3,19 @@ import React, {useEffect, useState} from "react"
 
 
 const Perfil = () => {
+
+    const [cuentaLikes, setCuentaLikes] = useState(0)
+
+    const handleCuenta = () => {
+        setCuentaLikes(cuentaLikes + 1)
+    }
+
     return (
-        <h1>Perfil</h1>
+        <>
+            <h1>Perfil</h1>
+            <button type="type" onClick={handleCuenta}>Dar Like</button>
+            <p> Conteo de Likes : {cuentaLikes}</p>
+        </>
     )
 }
 
